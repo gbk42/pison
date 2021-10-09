0c609ad1103ffc4c245ebed1d0e4ffe876db7420
Geoffrey Keating <gbk42@cornell.edu>
2021-10-09 00:20:12 -0400

    Simple feature analysis running

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,10 +1,11 @@
 """Module containing feature extraction framework functions"""
 
+import itertools
+from typing import Callable, Dict, Iterator, List, Literal, Optional, Tuple, Union
+
 import attr
-from numpy.lib.function_base import interp
-import pandas as pd
 import numpy as np
-from typing import Literal, Optional, Callable, Iterator, List, Tuple, Dict, Union
+import pandas as pd
 
 
 @attr.s(auto_attribs=True)
@@ -73,17 +74,47 @@ class FeatureExtractor:
                 start += self.interval
 
 
+def zero_crossings(data_segment: np.ndarray) -> float:
+    """Count the number of zero crossings in a data segment
+
+    Adapted from https://stackoverflow.com/questions/3843017/efficiently-detect-sign-changes-in-python
+    """
+    return len(np.where(np.diff(np.sign(data_segment)))[0])
+
+
+def mean_crossings(data_segment: np.ndarray) -> float:
+    """Count the number of mean crossings in the segment"""
+    return len(np.where(np.diff(np.sign(data_segment - data_segment.mean())))[0])
+
+
+def peak_frequency(data_segment: np.ndarray) -> float:
+    """Identify the bin of the peak frequency in an fft defined by the segment length of the extractor"""
+    fft_out = np.abs(np.fft.fft(data_segment - np.mean(data_segment)))
+    return np.argmax(fft_out[: (len(fft_out) // 2) + 1])
+
+
 def process_dataframe(
     dataset: pd.DataFrame,
     column_extractors: Dict[str, Union[FeatureExtractor, List[FeatureExtractor]]],
+    normalize: bool = True,
 ):
     if not set(column_extractors.keys()).issubset(set(dataset.columns)):
         raise ValueError("Extractors must target columns of the dataframe provided")
     # Apply the extractors to each column of data in the dataframe
-    all_features = {}
-    for column, extractors in column_extractors.items():
-        if not isinstance(extractors, list):
-            extractors = [extractors]
-        for extractor in extractors:
-            all_features.update(extractor(dataset.loc[:, column], column))
-    return all_features
+    results = pd.DataFrame()
+    for observation in itertools.product(range(5), range(1, 4)):
+        obs_features = {}
+        for column, extractors in column_extractors.items():
+            if not isinstance(extractors, list):
+                extractors = [extractors]
+            for extractor in extractors:
+                obs_features.update(
+                    extractor(dataset.loc[observation, column].values, column)
+                )
+
+        results = results.append(obs_features, ignore_index=True)
+
+    if normalize:
+        results = (results - results.mean()) / results.std()
+        results.dropna(axis=1, inplace=True)
+    return results
--- helpers.py
+++ helpers.py
@@ -1,6 +1,14 @@
+"""Module containing basic helper functions"""
 import pandas as pd
 from pathlib import Path
 from matplotlib import pyplot as plt
+import itertools
+
+EMG_CHANNELS = ["ch0", "ch1"]
+EMG_HP_CHANNELS = ["ch0_hp", "ch1_hp"]
+IMU_CHANNELS = ["qx", "qy", "qz", "qw"]
+GYRO_CHANNELS = ["gx", "gy", "gz"]
+ACC_CHANNELS = ["ax", "ay", "az"]
 
 body_movement_code = {
     0: "standing #1",
@@ -11,13 +19,18 @@ body_movement_code = {
 }
 
 
-def load_data() -> pd.DataFrame:
+def load_data(uniform_length: bool = True) -> pd.DataFrame:
     """Load and reindex the dataset to get each "gesture" in MultiIndex format
+
+    Args:
+        uniform_length: Find the minimum length of each observation and crop the rest to this length
 
     Returns:
         Pandas dataframe indexed by the body state label and repetition
     """
-    data_path = Path().absolute().joinpath('data').joinpath("pison_data_interview 2.csv")
+    data_path = (
+        Path().absolute().joinpath("data").joinpath("pison_data_interview 2.csv")
+    )
     columns = [
         "time_ms",
         "ch0",
@@ -42,4 +55,17 @@ def load_data() -> pd.DataFrame:
     dataset.set_index(["body_label", "rep"], inplace=True)
     # Sort multi-index to avoid performance warnings
     dataset.sort_index(inplace=True)
+    # Add a samples column and add it to the index
+    for observation in itertools.product(range(5), range(1, 4)):
+        dataset.loc[observation, "sample_num"] = range(len(dataset.loc[observation]))
+
+    min_length = min(
+        [
+            dataset.loc[combo].shape[0]
+            for combo in itertools.product(range(5), range(1, 4))
+        ]
+    )
+    if uniform_length:
+        print(f"Trimming observations to {min_length} samples")
+        dataset = dataset[dataset.sample_num < min_length]
     return dataset
